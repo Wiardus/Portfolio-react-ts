7dfac6436c594f37c61b7abc08f24b96cbbdfda5
Wiardus <ynte.braaksma@student.nhlstenden.com>
2021-12-15 11:13:52 +0100

    Added link to whisper app

--- src/Components/Work.tsx
+++ src/Components/Work.tsx
@@ -8,8 +8,8 @@ interface workProps {
 const Work = ({title, subtitle}: workProps) => 
 <div className="work">
     <h2 id="workTitle">{title}</h2>
-    <h3>{subtitle}</h3>
-    <a target="blank" href="https://github.com/Wiardus">
+    <a target="blank" href="https://whisper-chat-app.vercel.app/">Visit my social media app Whisper! <br /> Use the username Ynte and password 123 to login</a><a target="blank" href="https://github.com/Wiardus">
+    <h3>{subtitle}</h3>  
         <img id="github" src={github} alt="Github"/>
     </a>   
 </div>
