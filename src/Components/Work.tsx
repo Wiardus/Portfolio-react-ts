5d35a5b2f24c85258b3bd1e5cb4c0a3004b160e1
Wiardus <ynte.braaksma@student.nhlstenden.com>
2021-12-15 15:04:12 +0100

    Added guitar masters link

--- src/Components/Work.tsx
+++ src/Components/Work.tsx
@@ -9,6 +9,7 @@ const Work = ({title, subtitle}: workProps) =>
 <div className="work">
     <h2 id="workTitle">{title}</h2>
     <a target="blank" href="https://whisper-chat-app.vercel.app/">Visit my social media app Whisper! <br /> Use the username Ynte and password 123 to login</a><a target="blank" href="https://github.com/Wiardus">
+    <a target="blank" href="https://guitar-masters.vercel.app/">Visit my Guitar Masters app!</a>
     <h3>{subtitle}</h3>  
         <img id="github" src={github} alt="Github"/>
     </a>   
